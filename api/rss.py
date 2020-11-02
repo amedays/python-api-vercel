42d7f0e7ae9a08f5fa862e313b4c2fcf7c053276
amedays <72970432+amedays@users.noreply.github.com>
2020-11-02 21:05:58 +0800

    arrange

--- api/rss.py
+++ api/rss.py
@@ -1,84 +1,84 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from datetime import datetime
-# import urllib
-from urllib import parse
 from xml.dom import minidom as DOM
 import requests
 import io
-'''合并rss'''
-# TODO: 添加更多的筛选功能
-
-
-def get_rss_xml(url: str):
-    ''':param url: rss url\n
-    :return: xml string'''
-    res = requests.get(url)
-    return res.text
-
-
-def get_dom(xml: str):
-    tree = DOM.parseString(xml)
-    return tree
-
-
-def get_items_from_xml(tree):
-    ''':param xml: xml string\n
-    :return: items list'''
-    root = tree.documentElement
-    channel = root.getElementsByTagName("channel")
-    if channel:
-        channel = channel[0]
-        items = channel.getElementsByTagName("item")
-    return items if items else None
-
-
-class Item():
-    '''能够根据指定属性判断item重复'''
-
-    def __init__(self, node, diff="guid"):
-        self._node = node
-        self.diff = diff
-
-    def __eq__(self, other):
-        return self._node.getAttribute(self.diff) == other._node.getAttribute(self.diff)
+from urllib import parse
+# import urllib
 
 
 class handler(BaseHTTPRequestHandler):
+    '''合并rss'''
+    # TODO: 添加更多的筛选功能
+
     def do_GET(self):
-        path = self.path
-        query = parse.parse_qs(parse.urlsplit(path).query)
-        items = []
-        first = None
-        # 获取模板rss与items
-        if "rss" in query:
-            urls = query["rss"]
-            first = get_dom(get_rss_xml(urls[0]))
-            items = [item for item in get_items_from_xml(first)]
-            items_first = items.copy()
-            items = [Item(item) for item in items]
-            for url in urls[1:]:
-                xml = get_rss_xml(url)
-                tree = get_dom(xml)
-                tree_item = get_items_from_xml(tree)
-                for item in tree_item:
-                    item = Item(item)
-                    if item not in items:
-                        items.append(item)
-        if items:
-            items = [item._node for item in items]
-            root = first.documentElement
-            channel = root.getElementsByTagName("channel")
-            if channel:
-                channel = channel[0]
-                for item in items_first:
-                    channel.removeChild(item)
-                for item in items:
-                    channel.appendChild(item)
-        if first:
-            writer = io.StringIO()
-            first.writexml(writer, encoding="utf-8")
-        res = first.toprettyxml(
-            encoding="utf-8") if first else None
+        try:
+            class Item():
+                '''能够根据指定属性判断item重复'''
+
+                def __init__(self, node, diff="guid"):
+                    self._node = node
+                    self.diff = diff
+
+                def __eq__(self, other):
+                    return self._node.getAttribute(self.diff) == other._node.getAttribute(self.diff)
+
+            def get_rss_xml(url: str):
+                ''':param url: rss url\n
+                :return: xml string'''
+                res = requests.get(url)
+                return res.text
+
+            def get_dom(xml: str):
+                tree = DOM.parseString(xml)
+                return tree
+
+            def get_items_from_xml(tree):
+                ''':param xml: xml string\n
+                :return: items list'''
+                root = tree.documentElement
+                channel = root.getElementsByTagName("channel")
+                if channel:
+                    channel = channel[0]
+                    items = channel.getElementsByTagName("item")
+                return items if items else None
+
+            path = self.path
+            query = parse.parse_qs(parse.urlsplit(path).query)
+            items = []
+            first = None
+            # 获取模板rss与items
+            if "rss" in query:
+                urls = query["rss"]
+                first = get_dom(get_rss_xml(urls[0]))
+                items = [item for item in get_items_from_xml(first)]
+                items_first = items.copy()
+                items = [Item(item) for item in items]
+                for url in urls[1:]:
+                    xml = get_rss_xml(url)
+                    tree = get_dom(xml)
+                    tree_item = get_items_from_xml(tree)
+                    for item in tree_item:
+                        item = Item(item)
+                        if item not in items:
+                            items.append(item)
+            if items:
+                items = [item._node for item in items]
+                root = first.documentElement
+                channel = root.getElementsByTagName("channel")
+                if channel:
+                    channel = channel[0]
+                    for item in items_first:
+                        channel.removeChild(item)
+                    for item in items:
+                        channel.appendChild(item)
+            if first:
+                writer = io.StringIO()
+                first.writexml(writer, encoding="utf-8")
+            res = first.toprettyxml(
+                encoding="utf-8") if first else None
+        except Exception as e:
+            res = io.StringIO(str(e))
         self.send_response(200)
         self.send_header('Content-type', 'application/xml')
         self.end_headers()
